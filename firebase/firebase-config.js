39ffa9506cac879745c09c72c9494046a2ee9f9b
donmerengue <l.parma@hotmail.com>
2022-09-10 18:26:02 -0300

    fix firebaseConfig projectId

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -6,7 +6,7 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  projectId: "itesa-nft-game",
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
