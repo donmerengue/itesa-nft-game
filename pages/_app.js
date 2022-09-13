f8801470be3096f246ff584f1120fbe5aa094025
Marcos Facundo Rueda Adanto <marcos.fac.rueda@gmail.com>
2022-09-13 11:25:52 -0300

    instalación de chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import { Provider } from "react-redux";
 import store from "../state/store";
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Provider store={store}>
+      <ChakraProvider>
         <Component {...pageProps} />
+        </ChakraProvider>
       </Provider>
     </>
   );
