53cb35985466af8d5d41c3018fd9f130c3f57ab2
Lino <limbertlino@gmail.com>
2022-09-13 16:25:48 -0400

    Completadas las funcionalidades basicas para interactuar con el token
    - Pruebas de las funciones en la pagina testToken.js
    - Las funciones se importan desde utils/blockchain/tokenOperations.js
    - Se aniade la funcion para crear nueva address
    - Se corrige error de la funcion sendTokens

--- utils/blockchain/borrador.js
+++ utils/blockchain/borrador.js
@@ -46,8 +46,6 @@
 // }
 
 
-
-
 // // SEND BNB
 
 // const sender = "0x52Ec083D30192691872B60334bFDd1450C1826d9"
@@ -80,84 +78,6 @@
 //     console.log(error.message)
 //   }
 // }
-
-// // SIGNED OPERATIONS TOKEN
-
-
-// const sender = "0x52Ec083D30192691872B60334bFDd1450C1826d9"
-// const recipient = "0x39906C8A5D39fc920DF46b2aCeDc1B80e75E5b50"
-// const BEP20_ABI = abi
-
-// const senderKey =
-//   "8b9d24eae4dd47e51544868ec4056fa2ad305168a8f571c31b68d580aca89c94"
-
-// // Para firmar transacciones
-// const wallet = new ethers.Wallet(senderKey, bscProvider)
-// // Address del smart contract del token IGTX
-// const addressContract = "0x27D7F516Ff969d67170035d0a2B1F071859F602e"
-// // conexion al IGTX
-// const contract = new ethers.Contract(addressContract, BEP20_ABI, wallet)
-
-// // const contract = new ethers.Contract(addressContract, BEP20_ABI, bscProvider)
-
-// const main = async () => {
-//   try {
-//     // Conexion a la wallet que tiene el token
-//     // const contractWithWallet = contract.connect(wallet)
-//     // transaccion
-//     const tx = await contract.transfer(recipient, ethers.utils.parseEther("2"))
-//     // esperamos que sea minado en la blockchain
-//     await tx.wait()
-
-//     console.log(tx)
-//   } catch (error) {
-//     console.log(error.message)
-//   }
-// }
-
-// // INTERACCION CON METAMASK
-
-// const prueba = () => {
-//   const [account, setAccount] = useState(0)
-//   const [balance, setBalance] = useState(0)
-
-//   async function requestAccount() {
-//     try {
-//       const accounts = await window.ethereum.request({
-//         method: "eth_requestAccounts",
-//       })
-//       setAccount(accounts)
-//     } catch (error) {
-//       console.log(error.message)
-//     }
-//   }
-
-//   const bscProvider = new ethers.providers.JsonRpcProvider(
-//     "https://data-seed-prebsc-1-s1.binance.org:8545/",
-//     { name: "binance test-net", chainId: 97 }
-//   )
-
-//   const getBalan = async () => {
-//     try {
-//       const balance = await bscProvider.getBalance(`${account}`)
-//       console.log(ethers.utils.formatEther(balance))
-//       setBalance(ethers.utils.formatEther(balance))
-//     } catch (error) {
-//       console.log(error.message)
-//     }
-//   }
-
-//   return (
-//     <>
-//       <button onClick={requestAccount}>adquirir</button>
-//       <button onClick={getBalan}>balance</button>
-//       <p>{account}</p>
-//       <p>{balance}</p>
-//     </>
-//   )
-// }
-
-// export default prueba
 
 
 // // CREACION DE WALLET
