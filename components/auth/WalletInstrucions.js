8973a7d6b6a59e3ca3ae33f37b09fde9cf204120
Marcos Facundo Rueda Adanto <marcos.fac.rueda@gmail.com>
2022-09-14 18:24:55 -0300

    error solucionado

--- components/auth/WalletInstrucions.js
+++ components/auth/WalletInstrucions.js
@@ -1,23 +1,11 @@
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { updateData } from "../../fetchData/controllers";
-import { auth } from "../../firebase/firebase-config";
-import { createWallet } from "../../utils/blockchain/tokenOperations";
+import React from "react";
 
 const WalletInstrucions = () => {
-  const user = useSelector((state) => state.user);
 
-//   const { address, mnomic } = createWallet();
-
-//   updateData("users", auth.currentUser.uid, {
-//     walletAddress: address,
-//     mnomic,
-//   });
 
   return (
     <>
-      <div>Wallet Address : {address}</div>
-      <div>Wallet mnomic : {mnomic}</div>
+<Text>WalletInstrucions</Text>
     </>
   );
 };
