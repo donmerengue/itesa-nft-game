0418b2d3b3fb72d6af78002457eb0c03e88fa307
donmerengue <l.parma@hotmail.com>
2022-09-15 20:30:07 -0300

    page+component 404; Image en Navbar; Head en _app

---
+++ components/NotFound.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+
+
+const NotFound = () => {
+  const router = useRouter(); 
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 30000);
+  }, []);
+
+  return (
+    <div className="not-found">
+      <h1>Page not found...</h1>
+      <h2>
+        Go back{" "}
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </h2>
+    </div>
+  );
+};
+
+export default NotFound;
--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -13,16 +13,20 @@ import { useDispatch, useSelector } from "react-redux";
 import { requestAccount } from "../../utils/blockchain/tokenOperations";
 import { logoutUser } from "../../state/user";
 import useAuth from "../../hooks/useAuth";
+import Image from "next/image";
 
 const Navbar = () => {
   const [account, setAccount] = useState("");
   const dispatch = useDispatch();
+
   //Traer info del usuario logueado
   useAuth();
-
   const user = useSelector((state) => state.user);
-   //Manejo de cuenta de Metamask
-  const handleAccount = async () =>account ? setAccount(null) : setAccount(await requestAccount());
+  console.log("user state Redux desde navbar", user);
+
+  //Manejo de cuenta de Metamask
+  const handleAccount = async () =>
+    account ? setAccount(null) : setAccount(await requestAccount());
 
   //Manejo del logout
   const handlerLogout = () => {
@@ -30,30 +34,33 @@ const Navbar = () => {
   };
 
   return (
-    <Box bg={"gray.900"} color={"gray.50"} px={4} >
+    <Box bg={"gray.900"} color={"gray.50"} px={4}>
       <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
         <Stack direction="row" spacing={4} align="center">
           <Link href="/">
             <Button colorScheme="gray.50" variant="ghost">
              INTERGALAXY
             </Button>
+            {/* <Image
+              src="/jupiter.png"
+              alt="intergalaxy logo"
+              height={64}
+              width={64}></Image> */}
           </Link>
           <Button colorScheme="gray.50" variant="ghost">
-         MARKETPLACE
+            MARKETPLACE
           </Button>
           <Button colorScheme="gray.50" variant="ghost">
             PLAY NOW
           </Button>
         </Stack>
         <Stack direction="row" spacing={4} align="center">
-    
           {user ? (
             <Link href="/">
               <Button
                 colorScheme="gray.50"
                 variant="ghost"
-                onClick={handlerLogout}
-              >
+                onClick={handlerLogout}>
                 LOGOUT
               </Button>
             </Link>
@@ -65,20 +72,22 @@ const Navbar = () => {
             </Link>
           )}
 
-          {user? (
+          {user ? (
             <>
-            <Link href="/user/profile">
-              <Button colorScheme="gray.50" variant="ghost">
-               PROFILE
-              </Button>
-            </Link>
-            <Link href="/user/history">
-              <Button colorScheme="gray.50" variant="ghost">
-                HISTORY
-              </Button>
-            </Link>
+              <Link href="/user/profile">
+                <Button colorScheme="gray.50" variant="ghost">
+                  PROFILE
+                </Button>
+              </Link>
+              <Link href="/user/history">
+                <Button colorScheme="gray.50" variant="ghost">
+                  HISTORY
+                </Button>
+              </Link>
             </>
-          ):""}
+          ) : (
+            ""
+          )}
 
           <Flex alignItems={"center"}>
             <Menu>
@@ -88,8 +97,7 @@ const Navbar = () => {
                 variant={"link"}
                 cursor={"pointer"}
                 minW={0}
-                onClick={handleAccount}
-              >
+                onClick={handleAccount}>
                 {account ? (
                   <p className="text-white">{account[0].slice(0, 8)}</p>
                 ) : (
