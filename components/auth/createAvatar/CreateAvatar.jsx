d42fed54b5e2db55c678a261fb27e165eeddc5b7
Francisco <francisco.leyescampoy@gmail.com>
2022-09-20 18:43:27 -0300

    se arreglo el deploy

--- components/auth/createAvatar/CreateAvatar.jsx
+++ components/auth/createAvatar/CreateAvatar.jsx
@@ -14,13 +14,13 @@ const CreateAvatar = () => {
   return (
     <>
       <div className="flex">
-        <section class="bg-white dark:bg-gray-900">
-          <div class="container px-6 py-10 mx-auto">
-            <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
+        <section className="bg-white dark:bg-gray-900">
+          <div className="container px-6 py-10 mx-auto">
+            <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
               Avatars
             </h1>
             <p>Select your avatar to start this adventure</p>
-            <div class="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2">
+            <div className="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2">
               {avatars.map((avatar, i) => (
                 <Avatars avatar={avatar} key={i} />
               ))}
