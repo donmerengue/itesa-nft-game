26981378d92d0e98d1898a3b44ae4cc7eccf02b5
donmerengue <l.parma@hotmail.com>
2022-09-20 10:43:48 -0300

    logica proxy  matchmaking

--- utils/gameplay/levelUp.js
+++ utils/gameplay/levelUp.js
@@ -1,6 +1,5 @@
-
-// El nivel de experiencia para pasar de nivel es 10 veces el nivel actual
-export const exportexperiencePerLevel = (level) => 10 * level;
+// La experiencia necesaria para pasar de nivel es 10 veces el nivel actual
+export const experiencePerLevel = (level) => 10 * level;
 
 // Cada batalla ganada suma un punto de experiencia
 export const experiencePerBattle = (experience) => experience++;
