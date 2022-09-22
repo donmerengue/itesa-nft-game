d9228500285122232edbee8d7a73d36e640b25f9
donmerengue <l.parma@hotmail.com>
2022-09-22 11:55:12 -0300

    wannaPlay

--- pages/play.js
+++ pages/play.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ArenaOne from "../components/arenas";
+import ArenaCopy from "../components/arenas/ArenaCopy";
 
 const PlayPage = () => {
-  return <ArenaOne />;
+  return <ArenaCopy />;
 };
 
 export default PlayPage;
