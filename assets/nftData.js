1ef0af1611d16c0a6cca2c768e286155ee380225
mechiramella <ramellamercedes@gmail.com>
2022-09-23 14:52:53 -0300

    Marketplace dinamico

--- assets/nftData.js
+++ assets/nftData.js
@@ -1,35 +1,48 @@
 const nftData = [
   {
     name: "Sword",
-    description:'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
-    type: "Attack",
-    power: 3,
-    material: "Steel",
-    handle:"Gold",
+    description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
+    attributes: [
+      {
+        type: "Attack",
+        power: 3,
+        material: "Steel",
+        handle: "Gold"
+      }
+    ],
     img: "https://imgur.com/Y9USM7O.jpg",
     price: 40,
+
   },
   {
     name: "Circle Shield",
-    description:'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
-    type: "Attack",
-    power: 8,
-    material: "Steel, Wood and Bronze",
+    description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
+    attributes: [
+      {
+        type: "Attack",
+        power: 8,
+        material: "Steel, Wood and Bronze"
+      }
+    ],
     img: "https://imgur.com/6kTNafw.jpg",
     price: 20,
+
   },
   {
     name: "Reinforced Circular Shield",
-    description:'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
-    type: "Attack",
-    power: 10,
-    material: "Steel, Wood and Bronze",
+    description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
+    attributes: [{
+      type: "Attack",
+      power: 10,
+      material: "Steel, Wood and Bronze"
+    }],
     img: "https://imgur.com/W1EmPdn.jpg",
     price: 30,
   },
   {
     name: "Rectangle Shield",
-    description:'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
+    description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
+    attributes: [],
     type: "Attack",
     power: 10,
     material: "Steel, Wood and Bronze",
@@ -38,61 +51,71 @@ const nftData = [
   },
   {
     name: "Horned Helmet",
-    description:'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
-    type: "Attack",
-    power: 12,
-    material: "Steel, Wood and Animal's horns",
+    description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
+    attributes: [{
+      type: "Attack",
+      power: 12,
+      material: "Steel, Wood and Animal's horns"
+    }],
     img: "https://imgur.com/kezmcKX.jpg",
     price: 35,
   },
   {
     name: "Horned",
-    description:'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
-    type: "Luck",
-    power: 10,
-    material: "Steel and Animal's horns",
+    description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
+    attributes: [{
+      type: "Luck",
+      power: 10,
+      material: "Steel and Animal's horns"
+    }],
     img: "https://imgur.com/IxOpo57.jpg",
     price: 45,
   },
   {
     name: "Beer Chop",
-    description:'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
-    type: "Luck",
-    power: 8,
-    material: "Steel and Wood",
+    description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
+    attributes: [{
+      type: "Luck",
+      power: 8,
+      material: "Steel and Wood"
+    }],
     img: "https://imgur.com/l9s2yCI.jpg",
     price: 40,
   },
   {
     name: "Medieval Blackjack",
-    description:'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
-    type: "Defense",
+    description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
+    attributes: [{ type: "Defense",
     power: 9,
-    material: "Steel and Wood",
+    material: "Steel and Wood"}],
     img: "https://imgur.com/4AWLMl4.jpg",
     price: 35,
   },
   {
     name: "Hammer",
-    description:'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
-    type: "Defense",
-    power: 9,
-    material: "Steel and Wood",
+    description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
+    attributes: [{
+      type: "Defense",
+      power: 9,
+      material: "Steel and Wood"
+    }],
     img: "https://imgur.com/Flv8zkC.jpg",
     price: 35,
   },
   {
     name: "Axe",
-    description:'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
-    type: "Defense",
-    power: 7,
-    material: "Steel and Wood",
+    description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor',
+    attributes: [{
+      type: "Defense",
+      power: 7,
+      material: "Steel and Wood"
+    }],
     img: "https://imgur.com/6a7Ihma.jpg",
     price: 40,
   },
 ]
 
-export default nftData; 
+export default nftData;
 
 
 
