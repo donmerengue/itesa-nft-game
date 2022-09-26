233f101471ab07c82d146601b8caa05fce07fa65
Lino Mattos <limbertlino@gmail.com>
2022-09-26 15:17:49 -0400

    Interaccion de la web con los NFTs
    - Agregados assets de muestra para la creacion de los NFTs
    - Agregado ejemplo de datos fetcheados
    - Agregada funcion que recupera todos los NFTs de un determinado
      contrato

---
+++ pages/testNfts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TestNfts = () => {
+  return (
+    <div>testNfts</div>
+  )
+}
+
+export default TestNfts
---
+++ utils/blockchain/nftFetch.js
@@ -0,0 +1,25 @@
+const axios = require("axios")
+const API_KEY =
+  "PxmqNMtrjNPYT1Jafaq0ZCDqerDwTzvSt0Bx81SysC2FL3H2vXP3Vn2xKSfkkFoj"
+
+const options = {
+  method: "GET",
+  url: "https://deep-index.moralis.io/api/v2/nft/0xa8cf99020aF1BbfB904AB33a055C08354082DDe4?chain=bsc%20testnet&format=decimal",
+  headers: {
+    accept: "application/json",
+    "X-API-Key": API_KEY,
+  },
+}
+
+const getMetaData = async () => {
+  try {
+    const nftData = await axios.request(options)
+    const contractNfts = await nftData.data.result
+    return contractNfts
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+module.exports = { getMetaData }
