b065044b17b305e87d180bbf2fffccffddc38835
Marcos Facundo Rueda Adanto <marcos.fac.rueda@gmail.com>
2022-09-29 19:26:55 -0300

    datos de usuario

--- components/Profile/Profile.jsx
+++ components/Profile/Profile.jsx
@@ -7,18 +7,26 @@ import { getId } from "../../fetchData/controllers";
 import { Img } from "@chakra-ui/react";
 import Link from "next/link";
 import NtfBought from "./nftBought";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const UserProfile = () => {
   const [avatar, setAvatar] = useState({});
   const [stats, setStats] = useState("");
   const [activate, setActivate] = useState(false);
-  const handleAvatar = (id) => {
-    getId("userAvatar", id).then((res) => {
+  const user = useSelector((state) => state.user);
+
+
+  useEffect(()=>{
+
+    getId("userAvatar", auth.currentUser?.uid).then((res) => {
       setAvatar(res);
       setActivate(true);
-      setStats(id);
+      setStats(auth.currentUser?.uid);
     });
-  };
+
+  },[user])
+
 
   return (
     <div className="bg-[url('https://i.pinimg.com/originals/d7/d8/db/d7d8dbf3811c797e5d2cc364743ae3a7.gif')]">
@@ -29,20 +37,13 @@ const UserProfile = () => {
               <h2 className="text-2xl font-bold md:text-4xl">PROFILE</h2>
               <p className="lg:w-6/12 lg:mx-auto">
                 Here you can find your user stats and personal data! <br />
-                {activate ? (
+               
                   <Link href={"/user/edit"}>
                     <button className=" p-2 pl-5 pr-5 bg-violet-500 border-2 border-gray-900  transition-colors duration-700 transform hover:bg-gray-900 hover:text-gray-100 focus:border-4  focus:bg-gray-900 focus:text-white focus:border-white">
                       Edit User
                     </button>
                   </Link>
-                ) : (
-                  <button
-                    className=" p-2 pl-5 pr-5 bg-violet-500 border-2 border-gray-900  transition-colors duration-700 transform hover:bg-gray-900 hover:text-gray-100 focus:border-4  focus:bg-gray-900 focus:text-white focus:border-white"
-                    onClick={() => handleAvatar(auth.currentUser.uid)}
-                  >
-                    User
-                  </button>
-                )}
+               
               </p>
             </div>
             <div className="grid gap-12 lg:grid-cols-2">
@@ -61,7 +62,7 @@ const UserProfile = () => {
                     />
                     <div className="sm:w-7/12 pl-0 p-5">
                       <h4 className="text-2xl font-semibold bg-cyan-900 rounded-xl text-white">
-                        Press the user button
+                        Loading...
                       </h4>
                     </div>
                   </>
@@ -82,7 +83,7 @@ const UserProfile = () => {
                     />
                     <div className="sm:w-7/12 pl-0 p-5">
                       <h4 className="text-2xl font-semibold bg-cyan-900 rounded-xl text-white">
-                        Press the user button
+                        Loading...
                       </h4>
                     </div>
                   </>
@@ -91,7 +92,7 @@ const UserProfile = () => {
             </div>
           </div>
         </Container>
-        <NtfBought/>
+        <NtfBought />
       </div>
     </div>
   );
