946e9da9e1d76a0158b184d34248e4439330ab0c
donmerengue <l.parma@hotmail.com>
2022-09-29 18:32:15 -0300

    bug fix vista accesorios

--- components/play/UserAccessories.js
+++ components/play/UserAccessories.js
@@ -23,34 +23,48 @@ import {
   ModalFooter,
   useToast,
 } from "@chakra-ui/react";
-import { Fade, ScaleFade, Slide, SlideFade,useDisclosure } from '@chakra-ui/react'
+import {
+  Fade,
+  ScaleFade,
+  Slide,
+  SlideFade,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { equipNFTitem, getDocumento, updateData } from "../../fetchData/controllers";
+import {
+  equipNFTitem,
+  getDocumento,
+  updateData,
+} from "../../fetchData/controllers";
 import { auth } from "../../firebase/firebase-config";
 
 const GameAccessories = () => {
-const router = useRouter()
-const toast = useToast()
-const [loading, setLoading] = useState(false)
-const [betPerBattle, setBetPerBattle] = useState("")
+  const router = useRouter();
+  const toast = useToast();
+  const [loading, setLoading] = useState(false);
+  const [betPerBattle, setBetPerBattle] = useState("");
 
   const nftItems = useSelector((state) => state.nftItems);
-  
+
   const nftEquipped = useSelector((state) => state.nftEquipped);
   const user = useSelector((state) => state.user);
   //Cantidad de peleas diarias
-  const dailyMatches = useSelector(state=>state.dailyMatches)
+  const dailyMatches = useSelector((state) => state.dailyMatches);
 
-  const [initialDefense] = nftEquipped.filter((nft) => nft.type === "defense");
-  const [initialAttack] = nftEquipped.filter((nft) => nft.type === "attack");
+  const [initialDefense] = nftEquipped.filter(
+    (nft) => nft.type === "defense"
+  );
+  const [initialAttack] = nftEquipped.filter(
+    (nft) => nft.type === "attack"
+  );
   const [initialLuck] = nftEquipped.filter((nft) => nft.type === "luck");
 
   const defItems = nftItems.filter((nft) => nft.type === "defense");
   const attItems = nftItems.filter((nft) => nft.type === "attack");
   const luckItems = nftItems.filter((nft) => nft.type === "luck");
-  
+
   // Estados para elegir un nft item por tipo
   const [defense, setDefense] = useState(initialDefense?.id);
   const [anteriorDef, setAnteriorDef] = useState(initialDefense?.id);
@@ -59,9 +73,10 @@ const [betPerBattle, setBetPerBattle] = useState("")
   const [luck, setLuck] = useState(initialLuck?.id);
   const [anteriorLuck, setAnteriorLuck] = useState(initialLuck?.id);
 
-  
   useEffect(() => {
-    getDocumento("gameParams", "prizeParams").then(res=> setBetPerBattle(res.betPerBattle))
+    getDocumento("gameParams", "prizeParams").then((res) =>
+      setBetPerBattle(res.betPerBattle)
+    );
   }, []);
 
   // Setear los estados en la DB
@@ -101,32 +116,29 @@ const [betPerBattle, setBetPerBattle] = useState("")
     setLuck(e);
   };
 
-const handlerPlay = ( ) =>{
-  if(dailyMatches.length < 5){
-    router.push("/play")
-  }
-  else onOpen()
-}
+  const handlerPlay = () => {
+    if (dailyMatches.length < 5) {
+      router.push("/play");
+    } else onOpen();
+  };
 
-const handlerPlayAnyway = async () => {
+  const handlerPlayAnyway = async () => {
+    setLoading(true);
+    if (user.wannaBet) router.push("/play");
+    else {
+      toast({
+        title: "Error",
+        description: "Please allow the bet option",
+        status: "error",
+        position: "top",
+        duration: 5000,
+        isClosable: true,
+      });
+      setLoading(false);
+    }
+  };
 
-  setLoading(true)
-if(user.wannaBet) router.push("/play")
-else {
-  toast({
-    title: "Error",
-    description: "Please allow the bet option",
-    status: "error",
-    position: "top",
-    duration: 5000,
-    isClosable: true,
-  });
-  setLoading(false)
-}
-}
-
-  const { isOpen,onOpen,onClose, onToggle } = useDisclosure()
-   
+  const { isOpen, onOpen, onClose, onToggle } = useDisclosure();
 
   return (
     <WrapItem>
@@ -137,15 +149,15 @@ else {
             mb={10}
             role={"group"}
             p={6}
-            maxW={"350px"}
+            minW={"400px"}
+            maxW={"750px"}
             w={"full"}
             bg={"gray.800"}
             color={"white"}
             boxShadow={"2xl"}
             rounded={"lg"}
             pos={"relative"}
-            zIndex={1}
-          >
+            zIndex={1}>
             <Box maxW={"7xl"} columns={{ base: 1, md: 2 }} spacing={20}>
               <Heading
                 fontSize={"2xl"}
@@ -155,34 +167,36 @@ else {
                 textTransform={"uppercase"}
                 mb={"6"}
                 align={"center"}
-                justify={"center"}
-              >
+                justify={"center"}>
                 YOUR ACCESSORIES
               </Heading>
 
               <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
                 <List spacing={2}>
                   <Text
-                    fontSize={{ base: "8px", lg: "18px" }}
+                    fontSize={{ base: "8px", lg: "16px" }}
                     color={"white"}
                     textTransform={"uppercase"}
                     mb={"2"}
                     fontWeight={"bold"}
                     align={"left"}
-                    justify={"center"}
-                  >
+                    justify={"center"}>
                     Defense
                   </Text>
                   <RadioGroup
                     onChange={handlerDefense}
                     value={defense}
-                    defaultValue={defense}
-                  >
+                    defaultValue={defense}>
                     <Stack direction="row">
-                      {defItems.map((item) => {
-                       return <Radio
-                          value={item.id} key={item.id}
-                        >{`${item.type}: ${item.power}`}</Radio>;
+                      {defItems.map((item, i) => {
+                        if (i < 3)
+                          return (
+                            <Radio
+                              value={item.id}
+                              key={
+                                item.id
+                              }>{`${item.name}: ${item.power}`}</Radio>
+                          );
                       })}
                     </Stack>
                   </RadioGroup>
@@ -192,22 +206,26 @@ else {
               <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
                 <List spacing={2}>
                   <Text
-                    fontSize={{ base: "8px", lg: "18px" }}
+                    fontSize={{ base: "8px", lg: "16px" }}
                     color={"white"}
                     textTransform={"uppercase"}
                     mb={"2"}
                     fontWeight={"bold"}
                     align={"left"}
-                    justify={"center"}
-                  >
+                    justify={"center"}>
                     Attack
                   </Text>
                   <RadioGroup onChange={handlerAttack} value={attack}>
                     <Stack direction="row">
-                      {attItems.map((item) => {
-                        return <Radio
-                          value={item.id} key={item.id}
-                        >{`${item.type}: ${item.power}`}</Radio>;
+                      {attItems.map((item, i) => {
+                        if (i < 3)
+                          return (
+                            <Radio
+                              value={item.id}
+                              key={
+                                item.id
+                              }>{`${item.name}: ${item.power}`}</Radio>
+                          );
                       })}
                     </Stack>
                   </RadioGroup>
@@ -218,22 +236,26 @@ else {
               <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
                 <List spacing={2}>
                   <Text
-                    fontSize={{ base: "8px", lg: "18px" }}
+                    fontSize={{ base: "8px", lg: "16px" }}
                     color={"white"}
                     textTransform={"uppercase"}
                     mb={"2"}
                     fontWeight={"bold"}
                     align={"left"}
-                    justify={"center"}
-                  >
+                    justify={"center"}>
                     Luck
                   </Text>
                   <RadioGroup onChange={handlerLuck} value={luck}>
                     <Stack direction="row">
-                    {luckItems.map((item) => {
-                        return <Radio
-                          value={item.id} key={item.id}
-                        >{`${item.type}: ${item.power}`}</Radio>;
+                      {luckItems.map((item, i) => {
+                        if (i < 3)
+                          return (
+                            <Radio
+                              value={item.id}
+                              key={
+                                item.id
+                              }>{`${item.name}: ${item.power}`}</Radio>
+                          );
                       })}
                     </Stack>
                   </RadioGroup>
@@ -246,48 +268,60 @@ else {
           <VStack
             w={"full"}
             justify={"center"}
-            px={useBreakpointValue({ base: 4, md: 8 })}
-          >
+            px={useBreakpointValue({ base: 4, md: 8 })}>
             <Stack direction={"row"} justify={"center"}>
               <Link href="/">
                 <Button
                   bg={"gray.800"}
                   rounded={"full"}
                   color={"white"}
-                  _hover={{ bg: "blue.500" }}
-                >
+                  _hover={{ bg: "blue.500" }}>
                   HOME
                 </Button>
               </Link>
-                <Button
-                  bg={"gray.800"}
-                  rounded={"full"}
-                  color={"white"}
-                  _hover={{ bg: "blue.500" }}
-                  onClick={handlerPlay}
-                >
-                  PLAY
-                </Button>
-              </Stack>
-              <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader mr={6}>You have already made your 5 free daily fights available. </ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={5}>
-            <Divider/>
-          </ModalBody>
-<Text  p={6}>To fight again you can place a bet of <strong>{betPerBattle} ITGX</strong>. If you win, you get the reward, otherwise you lose your bet.</Text>
-<Text  p={6}>Are you sure you want to bet {betPerBattle} ITGX?</Text>
+              <Button
+                bg={"gray.800"}
+                rounded={"full"}
+                color={"white"}
+                _hover={{ bg: "blue.500" }}
+                onClick={handlerPlay}>
+                PLAY
+              </Button>
+            </Stack>
+            <Modal
+              closeOnOverlayClick={false}
+              isOpen={isOpen}
+              onClose={onClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader mr={6}>
+                  You have already made your 5 free daily fights available.{" "}
+                </ModalHeader>
+                <ModalCloseButton />
+                <ModalBody pb={5}>
+                  <Divider />
+                </ModalBody>
+                <Text p={6}>
+                  To fight again you can place a bet of{" "}
+                  <strong>{betPerBattle} ITGX</strong>. If you win, you get
+                  the reward, otherwise you lose your bet.
+                </Text>
+                <Text p={6}>
+                  Are you sure you want to bet {betPerBattle} ITGX?
+                </Text>
 
-          <ModalFooter>
-            <Button onClick={onClose}>Cancel</Button>
-            <Button colorScheme='blue' ml={3} onClick={handlerPlayAnyway} isLoading={loading}>
-              Play anyway
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+                <ModalFooter>
+                  <Button onClick={onClose}>Cancel</Button>
+                  <Button
+                    colorScheme="blue"
+                    ml={3}
+                    onClick={handlerPlayAnyway}
+                    isLoading={loading}>
+                    Play anyway
+                  </Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
           </VStack>
         </Box>
       </Center>
