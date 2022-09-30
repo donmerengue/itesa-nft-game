fa628e0fb8867b4e82842d5384d276ba19d6b1f9
mechiramella <ramellamercedes@gmail.com>
2022-09-30 13:32:42 -0300

    style Avatars

--- components/auth/createAvatar/Avatars.jsx
+++ components/auth/createAvatar/Avatars.jsx
@@ -1,4 +1,4 @@
-import { Img, useToast } from "@chakra-ui/react";
+import { Img, useToast, Wrap, Center, WrapItem, Flex,Box, Stack, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { addNewDoc } from "../../../fetchData/controllers";
@@ -30,7 +30,7 @@ const Avatars = ({ avatar }) => {
         isClosable: true,
       });
       router.replace("/");
-    }else{
+    } else {
       toast({
         title: "Error creating Avatar",
         description: "Please choose a name",
@@ -38,45 +38,109 @@ const Avatars = ({ avatar }) => {
         position: "top",
         duration: 5000,
         isClosable: true,
-      })
+      });
     }
   };
 
   return (
     <>
-      <div className="lg:flex">
-        <Img width={"20rem"} height={"16rem"} src={avatar.url} alt="" />
-        <div className="flex flex-col justify-between py-6 lg:mx-6">
-          {avatarName ? (
-            <p className="text-3xl font-semibold text-gray-800  dark:text-white ">
-              {avatarName}
-            </p>
-          ) : (
-            <form onSubmit={definitiveName}>
-              <input
-                type="search"
-                className="w-full border-black  px-4 py-1 text-gray-900 "
-                placeholder="Name your Avatar"
-                onChange={handleName}
-              />
-            </form>
-          )}
-          <span className="text-sm">SELECT YOUR AVATAR</span>
-          <button
-            onClick={() =>
-              handleAvatar({
-                img: avatar.url,
-                id: avatar.id,
-                name: avatarName,
-                userId: auth.currentUser.uid,
-              })
-            }
-            className="p-2 pl-5 pr-5 bg-transparent border-2 border-orange-500 text-orange-500 text-lg rounded-lg transition-colors duration-700 transform hover:bg-orange-600 hover:text-gray-100 focus:border-4 focus:border-indigo-300"
-          >
-            Select
-          </button>
-        </div>
-      </div>
+      <Center>
+        <Wrap ml={7} marginRight={10} columns={4}>
+          {/*      <div className="lg:flex"> */}
+          <WrapItem>
+            <Center>
+              <Box
+                mt={10}
+                mb={10}
+                role={"group"}
+                p={6}
+                maxW={"330px"}
+                w={"full"}
+                bg={"gray.800"}
+                color={"white"}
+                boxShadow={"2xl"}
+                rounded={"lg"}
+                pos={"relative"}
+                zIndex={1}
+                >
+                <Box
+                  rounded={"lg"}
+                  mt={1}
+                  pos={"relative"}
+                  height={"230px"}
+                  _after={{
+                    transition: "all .3s ease",
+                    content: '""',
+                    w: "full",
+                    h: "full",
+                    pos: "absolute",
+                    top: 5,
+                    left: 0,
+                    filter: "blur(10px)",
+                    zIndex: -1,
+                  }}
+                  _groupHover={{
+                    _after: {
+                      filter: "blur(10px)",
+                    },
+                  }}
+                >
+                  <Img
+                 /*    width={"20rem"}
+                    height={"16rem"} */
+                    rounded={"lg"}
+                    height={280}
+                    width={282}
+                    objectFit={"cover"}
+                    alt={""}
+                    src={avatar.url}
+                    bg={"white"}
+                  
+                  />
+                  </Box>
+                  <Stack pt={20} align={"center"}>
+
+                  {/*   <div className="flex flex-col justify-between py-6 lg:mx-6"> */}
+                  {avatarName ? (
+                    <Text align={"center"}
+                    color={"gray.500"}
+                    fontSize={"sm"}
+                    textTransform={"uppercase"}/* className="text-3xl font-semibold text-gray-800  dark:text-white " */>
+                      {avatarName}
+                    </Text>
+                  ) : (
+                    <form onSubmit={definitiveName}>
+                      <input
+                        type="search"
+                        className="w-full border-black  px-4 py-1 text-gray-900 "
+                        placeholder="Name your Avatar"
+                        onChange={handleName}
+                      />
+                    </form>
+                  )}
+
+                  <span className="text-sm">SELECT YOUR AVATAR</span>
+                  <button
+                    onClick={() =>
+                      handleAvatar({
+                        img: avatar.url,
+                        id: avatar.id,
+                        name: avatarName,
+                        userId: auth.currentUser.uid,
+                      })
+                    }
+                    className="p-2 pl-5 pr-5 bg-transparent border-2 border-orange-500 text-orange-500 text-lg rounded-lg transition-colors duration-700 transform hover:bg-orange-600 hover:text-gray-100 focus:border-4 focus:border-indigo-300" 
+                  >
+                    Select
+                  </button>
+                  {/* </div> */}
+                </Stack>
+              </Box>
+            </Center>
+          </WrapItem>
+          {/*   </div> */}
+        </Wrap>
+      </Center>
     </>
   );
 };
